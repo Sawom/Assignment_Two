9ae3d59923956becd6d3892699b18b1002557f7e
Sawom <asawom250@gmail.com>
2024-06-19 08:23:49 +0600

    added

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,6 @@ import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import { UserRoutes } from './app/modules/user/user.route';
 const app: Application = express();
-const port = 3000;
 
 // parsers
 app.use(express.json());
